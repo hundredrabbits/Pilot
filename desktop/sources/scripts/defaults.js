e88062a3525ae9167f57a9bcccfe8d7323fd460b
Ryan Ramage <ryan.ramage@gmail.com>
2019-01-21 17:37:19 -0700

    small changes

--- desktop/sources/scripts/defaults.js
+++ desktop/sources/scripts/defaults.js
@@ -1,4 +1,6 @@
 module.exports = [
-  require('../../synths/default/0.json'),
-  require('../../synths/default/1.json'),
+  require('../../synths/default/0.json')
+  , require('../../synths/default/1.json')
+  , require('../../synths/default/2.json')
+
 ]
