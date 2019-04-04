a961687dfb65d0f5d7ea3e690b275c5e91c65c12
Devine Lu Linvega <aliceffekt@gmail.com>
2019-04-04 13:56:11 +0900

    Started parsing

--- desktop/sources/scripts/synthetiser.js
+++ desktop/sources/scripts/synthetiser.js
@@ -3,7 +3,6 @@
 const Tone = require('tone')
 
 function Synthetiser (pilot) {
-
   this.synths = []
   this.synth = null
 
@@ -24,23 +23,30 @@ function Synthetiser (pilot) {
     pilot.terminal.log(data)
   }
 
-  this.parse = function(str){
-    return {type:'play', id:'C5'}
+  this.parse = function (msg) {
+    // Note
+    const channel = clamp(parseInt(msg.substr(0, 1)), 0, 16)
+    const octave = clamp(parseInt(msg.substr(1, 1)), 0, 8)
+    const note = msg.substr(2, 1)
+    return { type: 'play', channel: channel, octave: octave, note: note }
   }
 
-  this.operate = function(data){
+  this.operate = function (data) {
     this.synth.triggerAttackRelease('C5', 0.1)
   }
 
-  this.operate = function(data) {
-    if(data.type === 'play') {
+  this.operate = function (data) {
+    if (data.type === 'play') {
       this.play(data)
     }
   }
 
-  this.play = function(data){
-    this.synth.triggerAttackRelease(data.id, 0.1)
+  this.play = function (data) {
+    const name = `${data.note}${data.octave}`
+    this.synth.triggerAttackRelease(name, 0.1)
   }
+
+  function clamp (v, min, max) { return v < min ? min : v > max ? max : v }
 }
 
 module.exports = Synthetiser
